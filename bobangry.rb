e99d391f3b5e15cf27dcd4a1efee673f3af6eb3f
Kenneth Lay <pair+kenneth@howaboutwe.com>
2013-03-22 16:58:01 -0400

    Creates bobangry

---
+++ bobangry.rb
@@ -0,0 +1,24 @@
+ENV["RACK_ENV"] = ENV["RAILS_ENV"]
+require 'rubygems'
+require 'bundler/setup'
+require 'open-uri'
+Bundler.require
+
+URL = Sinatra::Base.test? ? 'http://localhost:3000' : 'http://bobanky.howaboutwe.com:3333'
+
+get '/bobangry' do
+  begin
+    xml = Nokogiri::XML(open("#{URL}/XmlStatusReport.aspx"))
+    xml_projects = xml.xpath("//Project")
+    @projects = xml_projects.map do |project|
+      {
+        name: project['name'],
+        success: project['lastBuildStatus'] == 'Success',
+        sha: project['lastBuildLabel']
+      }
+    end
+  rescue
+    @failure = true
+  end
+  haml :index
+end
