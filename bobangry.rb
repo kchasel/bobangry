74f73b597239798d3d066b2b1f1372daf973fc58
Kenneth Lay <pair+kenneth@howaboutwe.com>
2013-03-29 15:29:03 -0400

    Fixups

--- bobangry.rb
+++ bobangry.rb
@@ -4,21 +4,24 @@ require 'bundler/setup'
 require 'open-uri'
 Bundler.require
 
-URL = Sinatra::Base.test? ? 'http://localhost:3000' : 'http://bobanky.howaboutwe.com:3333'
 
-get '/bobangry' do
-  begin
-    xml = Nokogiri::XML(open("#{URL}/XmlStatusReport.aspx"))
-    xml_projects = xml.xpath("//Project")
-    @projects = xml_projects.map do |project|
-      {
-        name: project['name'],
-        success: project['lastBuildStatus'] == 'Success',
-        sha: project['lastBuildLabel']
-      }
+class Bobangry < Sinatra::Base
+  URL = Bobangry.test? ? 'http://localhost:3333' : 'http://bobanky.howaboutwe.com:3333'
+
+  get '/bobangry' do
+    begin
+      xml = Nokogiri::XML(open("#{URL}/XmlStatusReport.aspx"))
+      xml_projects = xml.xpath("//Project")
+      @projects = xml_projects.map do |project|
+        {
+          name: project['name'],
+          success: project['lastBuildStatus'] == 'Success',
+          sha: project['lastBuildLabel']
+        }
+      end
+    rescue
+      @failure = true
     end
-  rescue
-    @failure = true
+    haml :index
   end
-  haml :index
 end
